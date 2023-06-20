d76a5bd648cee63f6a5f9b45ee4b5c1f5ef948bc
aa-mill <94498640+aa-mill@users.noreply.github.com>
2023-06-19 20:56:47 -0400

    Adding npm and bootstrap

---
+++ js/app.js
@@ -0,0 +1,10 @@
+
+tsParticles
+    .loadJSON("tsparticles", "particles.json")
+    .then((container) => {
+        console.log("callback - tsparticles config loaded");
+    })
+    .catch((error) => {
+        console.error(error);
+    });
+
