0e8ec427ade25428888cf3f743cb58c16121a4b9
aa-mill <aaronmillerhockey21@gmail.com>
2023-07-30 22:29:25 -0400

    First finished draft

--- js/app.js
+++ js/app.js
@@ -8,3 +8,22 @@ tsParticles
         console.error(error);
     });
 
+// example starter JavaScript for disabling form submissions if there are invalid fields
+(() => {
+    'use strict'
+  
+    // fetch all the forms we want to apply custom bootstrap validation styles to
+    const forms = document.querySelectorAll('.needs-validation')
+  
+    // loop over them and prevent submission
+    Array.from(forms).forEach(form => {
+      form.addEventListener('submit', event => {
+        if (!form.checkValidity()) {
+          event.preventDefault()
+          event.stopPropagation()
+        }
+  
+        form.classList.add('was-validated')
+      }, false)
+    })
+  })()
